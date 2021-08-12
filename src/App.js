98258626ffa0fb29503c10ae072b44102e8d5272
Dmitriy <devilins24@gmail.com>
2021-08-12 20:14:25 +0300

    init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [word, setWord] = useState("");
+  const [link, setLink] = useState("");
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://api.giphy.com/v1/gifs/translate?api_key=${process.env.REACT_APP_WEATHER_API_KEY}&s=${word}`
+      );
+      const data = await response.json();
+      setLink(data.data.images.original.url);
+      setLoading(false);
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setWord(e.target.value);
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    fetchData();
+  };
+
+  const loader = <div className="loader"></div>;
+  let image = <img src={link} alt={word} />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      <h1 className="m-4">Gif Generator</h1>
+      <form onSubmit={submit} className="col-6 mx-auto px-5 input-group">
+        <input
+          type="text"
+          className="form-control "
+          onChange={handleChange}
+          value={word}
+          placeholder="cats"
+        />
+        <div className="input-group-append">
+          <button className="btn btn-primary " type="submit">
+            Search
+          </button>
+        </div>
+      </form>
+      <div className="d-flex justify-content-center m-4">
+        {loading ? loader : image}
+      </div>
     </div>
   );
 }
